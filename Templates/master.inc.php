9817a8354b93109179c42d3c6c6bc4d412537eb1
AlecBach <huntedstormbach@gmail.com>
2016-12-21 15:01:50 +1300

    Add content for home page, started work on footer.
    Additional changes: Added JS function to position certain text, cleaned up JS in general.

--- Templates/master.inc.php
+++ Templates/master.inc.php
@@ -34,24 +34,47 @@
           
           
         </div>
-        <div class="large-12 burger-menu columns fullWidth">
-          <ul class="menu vertical">
-            <li<?php if($page === "home"):?> class="active" <?php endif; ?>><a href="./">HOME</a></li>
-            <li<?php if($page === "blog"):?> class="active" <?php endif; ?>><a href="./?page=blog">BLOG</a></li>
-            <li<?php if($page === "about"):?> class="active" <?php endif; ?>><a href="./?page=about">ABOUT</a></li>
-            <li<?php if($page === "portfolio"):?> class="active" <?php endif; ?>><a href="./?page=portfolio">PORTFOLIO</a></li>
-            <li<?php if($page === "contact"):?> class="active" <?php endif; ?>><a href="./?page=contact">CONTACT</a></li>
-            <?php if(isset($_SESSION['email'])): ?><li<?php if($page === "account"):?> class="active" <?php endif; ?>><a href="./?page=account">ACCOUNT</a></li><?php endif; ?>
-            <li><a href="./?page=login"><?php if(isset($_SESSION['email'])): ?>LOG OUT<?php else: ?>LOG IN<?php endif; ?></a></li>
-          </ul>
-        </div>
+        
       <!-- </div> -->
     </div>
+    <div class="bump-for-nav"></div>
+    <div class="burger-menu row fullWidth">
+      <ul class="menu vertical">
+        <li<?php if($page === "home"):?> class="active" <?php endif; ?>><a href="./">HOME</a></li>
+        <li<?php if($page === "blog"):?> class="active" <?php endif; ?>><a href="./?page=blog">BLOG</a></li>
+        <li<?php if($page === "about"):?> class="active" <?php endif; ?>><a href="./?page=about">ABOUT</a></li>
+        <li<?php if($page === "portfolio"):?> class="active" <?php endif; ?>><a href="./?page=portfolio">PORTFOLIO</a></li>
+        <li<?php if($page === "contact"):?> class="active" <?php endif; ?>><a href="./?page=contact">CONTACT</a></li>
+        <?php if(isset($_SESSION['email'])): ?><li<?php if($page === "account"):?> class="active" <?php endif; ?>><a href="./?page=account">ACCOUNT</a></li><?php endif; ?>
+        <li><a href="./?page=login"><?php if(isset($_SESSION['email'])): ?>LOG OUT<?php else: ?>LOG IN<?php endif; ?></a></li>
+      </ul>
+    </div>
+
     <?php $this->content() ?>
 
     <div class="hero-image row fullWidth">
-      <div class="columns small-offset-1 small-10 medium-offset-5 medium-5 home-hero-text">
-        Welcome to my site. Feel free to explore my blog posts, check out my portfolio, learn about me, contact me or create an account!
+      <div class="columns small-offset-1 small-10 medium-offset-6 medium-5 home-hero-cont">
+        <div class="row home-hero-text xl-padding-right">
+          Welcome to my site. Feel free to explore my blog posts, check out my portfolio, learn about me, contact me or create an account!
+        </div>
+      </div>
+    </div>
+    <div class="row expanded main bump-medium">
+      <div class="columns medium-offset-1 medium-10 xl-padding">
+        <span>Hiya!</span><hr>I am a web specialist based in the capital of New Zealand, Wellington. I can provide front end design and server side programming. On this site I have many sites that I have already completed,  my blog where I post irregularly about my opinions &amp; random stuff and a small page dedicated to information on me. Please dive in and enjoy the works you see!
+        <div class="row expanded bump-small">
+          <div class="columns small-12 medium-4">To read more about me and my journey through life, explore <a href="">here!</a></div><div class="columns small-12 medium-4">To see what I've been up to lately, take a look at my blog <a href="">here!</a></div><div class="columns small-12 medium-4">If you're bored of me already, You can check out my sites <a href="">here!</a></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row expanded" id="footer">
+      <div class="columns medium-offset-1 medium-10 xl-padding">
+        <div class="row expanded bump-large">
+          <div class="columns medium-4 small-6">hey this is my footer content i hope u enjoy buddy</div>
+          <div class="columns medium-4 small-6">hey this is my footer content i hope u enjoy buddy</div>
+          <div class="columns medium-4 hide-for-small-only">hey this is my footer content i hope u enjoy buddy</div>
+        </div>
       </div>
     </div>
 
--- js/app.js
+++ js/app.js
@@ -4,26 +4,88 @@ $('.burger-cont').on('click', function(){
 	$('.nav').slideToggle();
 })
 
-// Set max width of nav to ensure perfect overflow collapsing. Jesus that took longer than expected.
-var headerWidth = 0;
+// Set max width of nav to ensure perfect overflow collapsing, set hero text position(margin) if on homepage.
+// var headerWidth = 0;
 var integerRemove = 50;
 var loggedIn = false;
 var totalWidth = 0;
 $(document).ready(function(){
 	$('.nav-item').each(function(i){
 		var width = $(this).outerWidth(true);
-		console.log(width);
 		totalWidth += width;
-		console.log(totalWidth);
 	})
 	if (totalWidth < 748){
 		loggedIn = false;
 	} else{
 		loggedIn = true;
 	}
-	console.log(loggedIn);
+	
+	//console.log('this is broken?:'+headerWidth);
+	positionHeroText();
+	setNavBar();
+	// if (loggedIn){
+	// 	if ($(window).outerWidth() < 1137) {
+	// 		$('.burgerOuter').removeClass('hide');
+	// 		integerRemove = 100;
+
+	// 	}else{
+	// 		$('.burgerOuter').addClass('hide');
+	// 		integerRemove = 40;
+	// 		$('.burger-menu').slideUp();
+	// 		$('.nav').slideDown();
+	// 	}
+	// }else{
+	// 	if ($(window).outerWidth() < 959) {
+	// 		$('.burgerOuter').removeClass('hide');
+	// 		integerRemove = 100;
+
+	// 	}else{
+	// 		$('.burgerOuter').addClass('hide');
+	// 		integerRemove = 40;
+	// 		$('.burger-menu').slideUp();
+	// 		$('.nav').slideDown();
+	// 	}
+	// }
+	// var fullWidth = $('#nav-container').innerWidth();
+	// var navWidth = fullWidth - headerWidth - integerRemove;
+	// $('.nav').css({"max-width": navWidth});
+	$(window).resize(function(){
+		positionHeroText();
+		setNavBar();
+		// if (loggedIn){
+		// 	if ($(window).outerWidth() < 1137) {
+		// 		$('.burgerOuter').removeClass('hide');
+		// 		integerRemove = 100;
+
+		// 	}else{
+		// 		$('.burgerOuter').addClass('hide');
+		// 		integerRemove = 40;
+		// 		$('.burger-menu').slideUp();
+		// 		$('.nav').slideDown();
+		// 	}
+		// }else{
+		// 	if ($(window).outerWidth() < 959) {
+		// 		$('.burgerOuter').removeClass('hide');
+		// 		integerRemove = 100;
+
+		// 	}else{
+		// 		$('.burgerOuter').addClass('hide');
+		// 		integerRemove = 40;
+		// 		$('.burger-menu').slideUp();
+		// 		$('.nav').slideDown();
+		// 	}
+		// }
+		// var fullWidth = $('#nav-container').innerWidth();
+		// var navWidth = fullWidth - headerWidth - integerRemove;
+		// $('.nav').css({"max-width": navWidth});
+		
+	})
+})
+function setNavBar(){
+	var	windowWidth = $(window).outerWidth();
+	var headerWidth = $('#header-text').outerWidth(true);
 	if (loggedIn){
-		if ($(window).outerWidth() < 1137) {
+		if (windowWidth < 1137) {
 			$('.burgerOuter').removeClass('hide');
 			integerRemove = 100;
 
@@ -34,7 +96,7 @@ $(document).ready(function(){
 			$('.nav').slideDown();
 		}
 	}else{
-		if ($(window).outerWidth() < 959) {
+		if (windowWidth < 966) {
 			$('.burgerOuter').removeClass('hide');
 			integerRemove = 100;
 
@@ -45,37 +107,15 @@ $(document).ready(function(){
 			$('.nav').slideDown();
 		}
 	}
-	
-	var headerWidth = $('#header-text').outerWidth(true);
 	var fullWidth = $('#nav-container').innerWidth();
 	var navWidth = fullWidth - headerWidth - integerRemove;
 	$('.nav').css({"max-width": navWidth});
-	$(window).resize(function(){
-		if (loggedIn){
-			if ($(window).outerWidth() < 1137) {
-				$('.burgerOuter').removeClass('hide');
-				integerRemove = 100;
-
-			}else{
-				$('.burgerOuter').addClass('hide');
-				integerRemove = 40;
-				$('.burger-menu').slideUp();
-				$('.nav').slideDown();
-			}
-		}else{
-			if ($(window).outerWidth() < 959) {
-				$('.burgerOuter').removeClass('hide');
-				integerRemove = 100;
-
-			}else{
-				$('.burgerOuter').addClass('hide');
-				integerRemove = 40;
-				$('.burger-menu').slideUp();
-				$('.nav').slideDown();
-			}
-		}
-		var fullWidth = $('#nav-container').innerWidth();
-		var navWidth = fullWidth - headerWidth - integerRemove;
-		$('.nav').css({"max-width": navWidth});
-	})
-})
+}
+function positionHeroText(){
+	var textBox = $('.home-hero-text');
+	var	textOffset = $(textBox).outerHeight() / 2;
+	var	contMiddle = $('.home-hero-cont').outerHeight() / 2;
+	var margin = contMiddle - textOffset;
+	console.log("(" + $(textBox).outerHeight() + " / 2 = " + textOffset + ") - (" + $('.home-hero-cont').outerHeight() + " / 2 = " + contMiddle + ") = " + margin);
+	$(textBox).css({"margin-top": margin+"px"});
+}
