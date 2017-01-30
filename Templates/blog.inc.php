bbaa9d9fde789f296589a4b8216ac9340b31ea03
AlecBach <huntedstormbach@gmail.com>
2017-01-30 21:23:28 +1300

    working on blog layout

--- Templates/blog.inc.php
+++ Templates/blog.inc.php
@@ -1,1 +1,33 @@
-u is find a blog
+<div class="row expanded">
+	<div class="blog-hero hero-image">
+		
+	</div>
+</div>
+<div class="row expanded">
+	<div class="columns medium-offset-1 medium-10 xl-padding account">
+		<div class="row expanded">
+			<div class="columns small-12">
+				<h1 style="margin-top: 15px;">Insert Blog Title.</h1>
+				<hr>
+			</div>
+		</div>
+		<div class="row expanded">
+			<div class="columns medium-9">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	
+			</div>
+			<div class="columns hide-for-small-only medium-3">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</div>
+</div>
